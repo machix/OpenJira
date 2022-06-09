0cb5ec5ac7d5691d97a4ea53b8902675c191e2b0
Marcelo Mazar <mmachix@gmail.com>
2022-06-09 00:20:51 -0300

    agrega entradas formulario

--- context/entries/EntriesContext.tsx
+++ context/entries/EntriesContext.tsx
@@ -3,6 +3,9 @@ import { Entry } from "../../interfaces";
 
 interface ContextProps {
   entries: Entry[]; // TODO: Falta el tipo de dato del arreglo
+
+  // Methods
+  addNewEntry: (description: string) => void;
 }
 
 export const EntriesContext = createContext({} as ContextProps);
