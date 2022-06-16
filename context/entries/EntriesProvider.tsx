512a5f1df0d338cd967db786e8a5f117f53b84ca
machix <mmachix@gmail.com>
2022-06-16 13:42:24 -0300

    creacion entrada frontend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
+
 import { EntriesContext, entriesReducer } from "./";
 import { Entry } from "../../interfaces";
 import { entriesApi } from "../../apis";
@@ -19,15 +19,10 @@ const Entries_INITIAL_STATE: EntriesState = {
 export const EntriesProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
-  const addNewEntry = (description: string) => {
-    const newEntry: Entry = {
-      _id: uuidv4(),
-      description,
-      createdAt: Date.now(),
-      status: "pending",
-    };
+  const addNewEntry = async (description: string) => {
+    const { data } = await entriesApi.post<Entry>("/entries", { description });
 
-    dispatch({ type: "[Entries] Add-Entry", payload: newEntry });
+    dispatch({ type: "[Entries] Add-Entry", payload: data });
   };
 
   const updateEntry = (entry: Entry) => {
